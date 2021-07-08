eee6dfa446d0efa9c0c1335d844aa0acc6fbe820
Gabriel <gabriellvieiras@gmail.com>
2021-07-08 19:16:32 -0300

    Add data splitting, data visualization, add whole experiment and add theta evaluation

--- stable/article_optimization.py
+++ stable/article_optimization.py
@@ -18,7 +18,7 @@ formatter = logging.Formatter(format_string)
 handler.setFormatter(formatter)
 LOGGER.addHandler(handler)
 
-
+# TO-DO: Change it to load any number of characteristics
 def load_data(data_path):
     """
     Load returns and firms characteristics from path.
@@ -90,7 +90,7 @@ def normalize_characteristics(firm_characteristics, characteristics_names):
     LOGGER.info("Normalized firm characteristics")
     return firm_characteristics
 
-
+# TO-DO: Change it to create any number of characteristics
 def create_characteristics(me_df, mom_df, btm_df, return_df, stocks_names):
     """
     Create firm characteristics complete dataframe
@@ -263,18 +263,131 @@ def create_w_benchmark(number_of_stocks, time):
     LOGGER.info("Created benchmark weights")
     return w_benchmark
 
+def data_split(size, train_percentage):
+    """
+    Define indexes for splitted train and test set using a split percentage.
+
+    Parameters
+    ----------
+    size: int
+        Total size of the data to be splitted.
+    train_percentage: float
+        Percentage between 0 and 1, where the training and testing data are splitted.
+    
+    Returns
+    -------
+    indexes_list: [(train_idxes, test_idxes)]
+        List of tuples containing training indexes and testing indexes.
+
+    """
+    train_size = int(size*train_percentage)
+    train_index =np.arange(train_size) 
+    test_index = np.arange(start=train_size, stop=size)
+    indexes_list = [(train_index, test_index)]
+    LOGGER.info(f'Train size: {train_size}, test size: {size-train_size}')
+    return indexes_list
+
+def plot_splitted_data(characteristic_df, characteristic_name, indexes_list, stock_name):
+    """
+    """
+    LOGGER.info("PLotting splitted data example.")
+    plt.figure(figsize=(12,8))
+    plt.title(f"{stock_name} Monthly {characteristic_name}")
+    plt.ticklabel_format(style='plain')
+    for train, test in indexes_list:
+        train_btm = characteristic_df.loc[train]
+        test_btm = characteristic_df.loc[test]
+        
+        train_btm[stock_name].plot(c='blue', label='train')
+        test_btm[stock_name].plot(c='orange', label='test')
+        plt.xlabel('Month')
+        plt.ylabel(characteristic_name)
+        plt.legend()
+    plt.savefig(f'./{stock_name}_monthly_{characteristic_name.lower()}.jpg')
+    plt.show()
+
+def evaluate_theta(sol_theta, test_me, test_mom, test_btm, test_return, stocks_names):
+    """
+    Evaluate theta on test sample.
+    """
+    LOGGER.info("Evaluating theta on test set.")
+    #### TESTING CHARACTERISTICS
+    firm_characteristics_test, r_test, time_test, number_of_stocks = create_characteristics(test_me, test_mom, test_btm, test_return, stocks_names)
+
+    #### CREATE BENCHMARK FOR TESTING
+    w_benchmark_test = create_w_benchmark(number_of_stocks, time_test)
+
+    benchmark_r_test_series=pd.Series(sum(w_benchmark_test*r_test)).describe()
+    benchmark_test_r = benchmark_r_test_series['mean']
+    benchmark_test_r_std = benchmark_r_test_series['std']
+
+    ### CREATE TEST WEIGHT AND FIND ITS RETURN
+    w_test = np.empty(shape=(number_of_stocks, time_test))
+    for i in range(number_of_stocks):
+        firm_df = firm_characteristics_test[i].copy()
+        firms_coeff = sol_theta.dot(firm_df.T)
+        w_test[i] = w_benchmark_test[i] + (1/number_of_stocks)*firms_coeff
+    
+    r_test_series = pd.Series(sum(w_test*r_test)).describe()
+    test_r = r_test_series['mean']
+    test_r_std = r_test_series['std']
+
+    LOGGER.info("Evalueted theta on test set.")
+    return benchmark_test_r, benchmark_test_r_std, test_r, test_r_std
+
+
+# TO-DO: Change it to use any number of characteristics
+def create_experiment(mcap, lreturn, book_to_mkt_ratio, monthly_return , stocks_names, indexes_list, risk_constant):
+    """
+    """
+    LOGGER.info("Started experiment.")
+    np.random.seed(123)
+    for train, test in indexes_list:
+        LOGGER.info("Splitting data into train and test set.")
+        theta0 = np.random.rand(1, 3)
+        
+        train_btm = book_to_mkt_ratio.loc[train]
+        train_me = mcap.loc[train]
+        train_mom = lreturn.loc[train]
+        train_return = monthly_return.loc[train]
+        
+        test_btm = book_to_mkt_ratio.loc[test]
+        test_me = mcap.loc[test]
+        test_mom = lreturn.loc[test]
+        test_return = monthly_return.loc[test]
+
+        #### TRAINING CHARACTERISTICS
+        firm_characteristics, r, time, number_of_stocks = create_characteristics(train_me, train_mom, train_btm, train_return, stocks_names)
+
+        ### Creating weights to a benchmark portifolio using uniform weighted returns
+        w_benchmark = create_w_benchmark(number_of_stocks, time)
+
+        ### CREATING RETURNS TO COMPARE
+        benchmark_mean_return = sum(sum(w_benchmark*r))/time
+
+        
+        sol, mean_obj_r, mean_r = optimizing_step(firm_characteristics, r, time, number_of_stocks, theta0, w_benchmark, risk_constant)
+        sol_theta = sol.x
+
+        ### Evaluate founded theta on test samples and find its mean return from optimized and benchmark.
+        benchmark_test_r, benchmark_test_r_std, test_r, test_r_std = evaluate_theta(sol_theta, test_me, test_mom, test_btm, test_return, stocks_names)
+
+        LOGGER.info("Finished experiment.")
+        return benchmark_mean_return, mean_obj_r, mean_r, benchmark_test_r, benchmark_test_r_std, test_r, test_r_std
+
 def main():
     data_path = "../data/"
     mcap, lreturn, book_to_mkt_ratio, monthly_return = load_data(data_path)
-    stock_names = list(monthly_return.columns)
-    firm_characteristics, r, time, number_of_stocks = create_characteristics(mcap, lreturn, book_to_mkt_ratio, monthly_return,stock_names)
-    w_benchmark = create_w_benchmark(number_of_stocks, time)
-    
-    np.random.seed(123)
+    stocks_names = list(monthly_return.columns)
+    total_size = monthly_return.shape[0]
+    train_split = 0.7
+    indexes_list = data_split(total_size, train_split)
+    # plot_splitted_data(monthly_return, 'Return', indexes_list, 'VALE3')
     risk_constant = 5
-    theta0 = np.random.rand(1, 3)
+    benchmark_mean_return, mean_obj_r, mean_r, benchmark_test_r, benchmark_test_r_std, test_r, test_r_std = create_experiment(
+        mcap, lreturn, book_to_mkt_ratio, monthly_return , stocks_names, indexes_list, risk_constant
+    )
 
-    sol, mean_obj_r, mean_r = optimizing_step(firm_characteristics, r, time, number_of_stocks, theta0, w_benchmark, risk_constant)
     print("Done")
 
 
